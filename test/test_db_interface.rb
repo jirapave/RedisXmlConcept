a9ba42715f5a7cb16f14674c20feaf6680256a64
Pavel Jira <Pavel@.(none)>
2011-11-19 17:51:44 +0100

    Test folder and first test just to show how it works in ruby

---
+++ test/test_db_interface.rb
@@ -0,0 +1,17 @@
+require_relative "../lib/base_interface/db_interface"
+require "test/unit"
+
+class TestDBInterface < Test::Unit::TestCase
+  def test_add_to_hash
+    #This will work here...but it is dangerous! $0 now refers to path to this file, so
+    #File.dirname($0) + '/../lib/' still works (same for main.rb), but if this file would
+    #ever be in some other folder..it will not work
+    db = BaseInterface::DBInterface.instance
+
+    hash = %w(field-1 value-1 field-2 value-2)
+    #Add to hash, overwrite all
+    db.add_to_hash "hash", *hash, true
+    result_hash = db.find_value "hash"
+    assert_equal(true, result_hash.length == 2)
+  end
+end
