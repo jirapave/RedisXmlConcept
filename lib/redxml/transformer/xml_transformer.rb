5b4abeee87257228c1872bb9df0bdca2bf8ffac9
jirapave <jirapave@fit.cvut.cz>
2012-04-05 20:23:53 +0200

    Optimalization reworked, plain text content now part of element key content along with attributes = cca 40% faster.

--- lib/redxml/transformer/xml_transformer.rb
+++ lib/redxml/transformer/xml_transformer.rb
@@ -11,10 +11,8 @@ module Transformer
   # as an init parameter). It supports saving and retrieving nodes recursively.
   class XMLTransformer
     
-    # Separator of attributes, those are saved in one string with separator as delimiter
-    ATTR_SEPARATOR = "\""
     # Separator of direct descendants of the certain element, used as delimiter
-    DESC_SEPARATOR = "|"
+    DESC_SEPARATOR = "<"
     
     # Creates new instance of XmlTransformer to work with document.
     # ==== Parameters
@@ -41,13 +39,18 @@ module Transformer
         namespace = ns_split[0]
         elem_name = ns_split[1]
       end
+      
+      desc_keys = @db_interface.get_hash_value(@key_builder.content_key, key_elem_builder.to_s)
+      parsed = parse_elem_desc(desc_keys)
+      attrs = parsed["attributes"]
+      part_keys = parsed["descendants"]
         
       # Attributes preparation
       attrs = @db_interface.get_hash_value(@key_builder.content_key, key_elem_builder.attr)
       fields_only = []
       values_only = []
       if attrs != nil
-        attrs.split(ATTR_SEPARATOR).each_with_index do |x, index|
+        attrs.split(DESC_SEPARATOR).each_with_index do |x, index|
           fields_only << @mapping_service.unmap_attr_name(x) if index%2 == 0
           values_only << x if index%2 != 0
         end
@@ -124,30 +127,22 @@ module Transformer
         #uri: http://www.neco.com/a
         #ns: [["b", "http://www.blbla.com/u"]] 
         
-      desc_keys = @db_interface.get_hash_value(@key_builder.content_key, key_elem_builder.to_s)
-      part_keys = []
-      if desc_keys != nil
-        desc_keys.split(DESC_SEPARATOR).each do |key|
-          part_keys << key
-        end
-      end
-        
-      if part_keys # if this element is not empty (like <element />
-        part_keys.each do |key_str|
-          type = Transformer::KeyElementBuilder.text_type(key_str)
-          if type
-            text_content = @db_interface.get_hash_value(@key_builder.content_key, key_str)
-            case type
-              when XML::TextContent::PLAIN
-                @handler.characters(text_content)
-              when XML::TextContent::COMMENT
-                @handler.comment(text_content)
-              when XML::TextContent::CDATA
-                @handler.cdata_block(text_content)
-            end
+      if part_keys
+        part_keys.each do |str|
+          str = str.split("-")
+          if str.length == 1
+            generate_node_sax_events(Transformer::KeyElementBuilder.build_from_s(@key_builder, str[0]))
           else
-            #Child element
-            generate_node_sax_events(Transformer::KeyElementBuilder.build_from_s(@key_builder, key_str))
+            case str[0].to_i
+              when XML::TextContent::PLAIN then
+                @handler.characters str[1]
+              when XML::TextContent::COMMENT then
+                text_content = @db_interface.get_hash_value(@key_builder.content_key, str[1])
+                @handler.comment text_content
+              when XML::TextContent::CDATA then
+                text_content = @db_interface.get_hash_value(@key_builder.content_key, str[1])
+                @handler.cdata_block text_content
+            end
           end
         end
       end
@@ -172,7 +167,7 @@ module Transformer
       root_key = info_hash["root"]
       #TODO generate start document and decaration
       root_key_builder = Transformer::KeyElementBuilder.build_from_s(key_builder, root_key)
-      @ns = [] #Contains declared namespaces
+      @ns = @db_interface.find_value(key_builder.namespace_key)
       @handler.start_document
       generate_node_sax_events(root_key_builder)
       @handler.end_document
@@ -183,102 +178,63 @@ module Transformer
     # ==== Parameters
     # * +node+ - XML::Node
     def save_node(node)
-      # Optimalization based on consolidation of attributes into descendant keys, they now
-      # share one key, which look as following:
-      # 2|1:2>1|1:2>2|attrId|attrValue|attrId|attrValue
-      # First number is used to determine count of descendants easily to enable smooth parsing.
+      # Optimalization based on consolidation of attributes and text content into descendant keys, 
+      # they now share one key, which look as following:
+      # 2|attrId|attrValue|attrId|attrValue|1:2>1|1:2>2|1-Ahoj jak se mate|3-1:2>1>d>1
+      # First number is used to determine number of attributes easily to enable smooth parsing.
+      # Note: Text content is is encoded into this string ONLY if it's plain String, when it's
+      # comment or CDATA, key is given, because they can contain any character, it would be
+      # impossible to encode them.
       
       main_hash = @key_builder.content_key
       key = node.database_key
-      descendant_keys = ""
+      elem_info = ""
       text_content = []
+      if node.attributes
+        elem_info << "#{node.attributes.attrs.length}" << DESC_SEPARATOR
+        node.attributes.attrs.each do |key, value|
+          elem_info << key << DESC_SEPARATOR << value << DESC_SEPARATOR
+        end
+      end
+      
       node.descendants.each do |desc|
-        if desc.instance_of? String
-          descendant_keys << desc << DESC_SEPARATOR
-        else
-          descendant_keys << desc.database_key << DESC_SEPARATOR
-          if desc.text_node?
-            #text_node is plain text, commnt and cdata, sax_parser is creating keys for them, we don't bother here
+        if desc.instance_of?(XML::TextContent)
+          # Note: Only basic text content can be encoded inside descedants, because comments
+          # and CDATA can contain any characters = impossible to decode, we can't know if it's
+          # still CDATA or new descedant
+          case desc.type
+          when XML::TextContent::PLAIN then
+            elem_info << "#{desc.type}-" << desc.text_content << DESC_SEPARATOR
+          else
+            elem_info << "#{desc.type}-" << desc.database_key << DESC_SEPARATOR
             text_content << desc.database_key 
             text_content << desc.text_content
           end
-        end
-      end
-      @profile = false
-      if node.attributes
-        node.attributes.attrs.each do |key, value|
-          if key.include?("ye") or value.include?("ye")
-            @profile = true
-          end
-          descendant_keys << key << DESC_SEPARATOR << value << DESC_SEPARATOR
-        end
-      end
-      
-      if descendant_keys != ""
-        descendant_keys = descendant_keys[0..-2] # Delete useless delimiter at the end
-        # Add number o descendants
-        if node.descendants
-        descendant_keys = "#{node.descendants.length}#{DESC_SEPARATOR}" + descendant_keys
         else
-          descendant_keys = "0#{DESC_SEPARATOR}" + descendant_keys
+          elem_info << desc << DESC_SEPARATOR
         end
-        @db_interface.add_to_hash(main_hash, [key, descendant_keys], true)
       end
       
-      
-      #Than we will save information about text nodes
       if(!text_content.empty?)
         @db_interface.add_to_hash(main_hash, text_content, true)
       end
-    end
-    
-    # Saves attributes of a given node in a database, remember that node itself know database key
-    # under which it should be saved.
-    # ==== Parameters
-    # * +node+ - XML::Node
-    # ==== Return value
-    # True if attributes were saved, False if there wasn't any attributes to save
-    def save_attributes(node)
-      attr_arr = []
-      iter = 0
-      node.attributes.attrs.each do |key, value|
-        attr_arr << key << value
-        iter +=  1
+      
+      if elem_info != ""
+        elem_info = elem_info[0..-2] # Delete useless delimiter at the end
+        @db_interface.add_to_hash(main_hash, [key, elem_info], true)
       end
-      attributes = attr_arr.join("#{ATTR_SEPARATOR}" )
-      attr_key = Transformer::KeyElementBuilder.build_from_s(@key_builder, node.database_key).attr
-      if !attributes.empty?
-        @db_interface.add_to_hash(@key_builder.content_key, [attr_key, attributes], true) 
-        return true
-      end
-      return false
     end
     
     # Get all attributes of the element specified by KeyElementBuilder.
+    # Note that getting whole element is as fast as this method.
     # ==== Parameters
     # * +key_elem_builder+ - KeyElementBuilder
     # * +mapped+ - Determines if the returned attributes contains real names or IDs
     # ==== Return value
     # Hash where field is name of the attribute and value is obviously value.
     def get_attributes(key_elem_builder, mapped=true)
-      attrs = @db_interface.get_hash_value(@key_builder.content_key, key_elem_builder.attr)
-        if attrs != nil
-          attrs_hash = {}
-          fields_only = []
-          values_only = []
-          attrs.split(ATTR_SEPARATOR).each_with_index do |x, index|
-            fields_only << x if index%2 == 0
-            values_only << x if index%2 != 0
-          end
-          #Now we have fields and values apart
-          fields_only.each_with_index do |field, index|
-            attr_name = field
-            #attr_name = @attr_mapping[field] if mapped
-            attr_name = @mapping_service.unmap_attr_name field if mapped
-            attrs_hash[attr_name] = values_only[index]
-          end
-        end
-        return attrs_hash
+      node = get_node(key_elem_builder, mapped)
+      return node.attributes
     end
     
     # Retrieves a document under certain key represented as a key parameter.
@@ -298,8 +254,6 @@ module Transformer
       doc = "<?xml version=\"#{info_hash["version"]}\" encoding=\"#{info_hash["encoding"]}\" standalone=\"#{info_hash["standalone"]}\" ?>"
       doc = Nokogiri::XML(doc)
       
-      @ns = @db_interface.find_value(key_builder.namespace_key)
-      
       builder = Nokogiri::XML::Builder.with(doc) do |xml|
         root_key = info_hash["root"]
         root_key_builder = Transformer::KeyElementBuilder.build_from_s(key_builder, root_key)
@@ -313,7 +267,8 @@ module Transformer
     # * +key_elem_builder+ - KeyElementBuilder instance to specify a node to be retrieved
     # ==== Return value
     # Nokogiri::XML::Node instance 
-    def get_node(key_elem_builder)
+    def get_node(key_elem_builder, mapped=true)
+      @mapped = mapped
       @ns = @db_interface.find_value(key_elem_builder.key_builder.namespace_key)
       builder = Nokogiri::XML::Builder.new do |xml|
         if @ns 
@@ -323,10 +278,43 @@ module Transformer
         end
         build_node(key_elem_builder, xml)
       end
+      @mapped = true
       return builder.doc.root
     end
     
+    # Parse element key content which can contain text along with key of descendants and
+    # attributes. 
+    # ==== Parameters
+    # * +desc_keys+ - String with content of element key (for example 1:2>1)
+    # * +mapped+ - True if attribtes should be mapped back into names, false otherwise
+    # ==== Return value
+    # Returns hash, where attributes are saved under field "attributes" in the form of hash
+    # and other parts are saved in the form of array under the field "descendants"
+    def parse_elem_desc(desc_keys, mapped=true)
+      attrs_hash = {}
+      part_keys = []
+      if desc_keys != nil
+        all = desc_keys.split(DESC_SEPARATOR)
+        attr_all = all[1..(all[0].to_i*2)]
+        part_keys = all[(all[0].to_i*2+1)..-1]
+        
+        # Now attributes
+        attr_all.each_slice(2) do |attr|
+          attr_name = attr[0]
+          #attr_name = @attr_mapping[field] if mapped
+          attr_name = @mapping_service.unmap_attr_name attr_name if mapped
+          attrs_hash[attr_name] = attr[1]
+        end
+      end
+      
+      result = {}
+      result["attributes"] = attrs_hash
+      result["descendants"] = part_keys
+      return result
+    end
+    
     private 
+    
     def get_elem_name(id)# :nodoc:
       mapping.each do |key, value|
         if (id == value)
@@ -344,80 +332,58 @@ module Transformer
     # * +xml+ - Context under which should the element be retrieved. This context can be created by using
     #           Nokogiri::XML::Builder.new { |xml|} or Nokogiri::XML::Builder.with(doc_or_elem) { |xml|}
     def build_node(key, xml)
-      # Optimalization based on consolidation of attributes into descendant keys, they now
-      # share one key, which look as following:
-      # 2|1:2>1|1:2>2|attrId|attrValue|attrId|attrValue
-      # First number is used to determine count of descendants easily to enable smooth parsing.
+      # Optimalization based on consolidation of attributes and text content into descendant keys, 
+      # they now share one key, which look as following:
+      # 2|attrId|attrValue|attrId|attrValue|1:2>1|1:2>2|1-Ahoj jak se mate|3-1:2>1>d>1
+      # First number is used to determine number of attributes easily to enable smooth parsing.
+      # Note: Text content is is encoded into this string ONLY if it's plain String, when it's
+      # comment or CDATA, key is given, because they can contain any character, it would be
+      # impossible to encode them.
       
-        elem_name = @mapping_service.unmap_elem_name key.elem_id
-        ns_split = elem_name.split(':')
-        namespace = nil
-        if(ns_split.length > 1)
-          namespace = ns_split[0]
-          elem_name = ns_split[1]
-        end
-        
-        desc_keys = @db_interface.get_hash_value(@key_builder.content_key, key.to_s)
-        attrs_hash = {}
-        part_keys = []
-          if desc_keys != nil
-            all = desc_keys.split(DESC_SEPARATOR)
-            desc_all = all[1..(all[0].to_i)]
-            attr_all = all[(all[0].to_i+1)..-1]
-            desc_all.each do |key|
-             part_keys << key
-            end
-            
-            # Now attributes
-            
-            fields_only = []
-            values_only = []
-            attr_all.each_with_index do |x, index|
-              fields_only << x if index%2 == 0
-              values_only << x if index%2 != 0
-            end
-            #Now we have fields and values apart
-            fields_only.each_with_index do |field, index|
-              attr_name = field
-              #attr_name = @attr_mapping[field] if mapped
-              attr_name = @mapping_service.unmap_attr_name field
-              attrs_hash[attr_name] = values_only[index]
+      
+      elem_name = @mapping_service.unmap_elem_name key.elem_id
+      ns_split = elem_name.split(':')
+      namespace = nil
+      if(ns_split.length > 1)
+        namespace = ns_split[0]
+        elem_name = ns_split[1]
+      end
+      desc_keys = @db_interface.get_hash_value(@key_builder.content_key, key.to_s)
+      parsed = parse_elem_desc(desc_keys)
+      attrs_hash = parsed["attributes"]
+      part_keys = parsed["descendants"]
+      #This will ad namespace to the current element in xml, raise ArgumentError if given namespace
+      #dos not exist = hasn't been defined in xmlns attribute   
+      xml[namespace] if namespace
+      #Creating element with a given name and attributes, xmlns attributes are automaticaly parsed
+      #as namesace declarations
+      # Note: Underscore is crucial here, it is used to disambiguate ruby internal calls
+      # for detailed information see: http://nokogiri.org/Nokogiri/XML/Builder.html
+      # section special tags
+      xml.send("#{elem_name}_", attrs_hash) do |elem_xml|
+        # This is crucial, Nokogiri uilder automatically adds namespaces into children
+        # if we don't explicily delete them
+        elem_xml.parent.namespace = nil unless namespace
+          if part_keys # if this element is not empty (like <element />
+            part_keys.each do |str|
+              str = str.split("-")
+              if str.length == 1
+                build_node(Transformer::KeyElementBuilder.build_from_s(@key_builder, str[0]), elem_xml)
+              else
+                case str[0].to_i
+                when XML::TextContent::PLAIN then
+                  elem_xml.text str[1]
+                when XML::TextContent::COMMENT then
+                  text_content = @db_interface.get_hash_value(@key_builder.content_key, str[1])
+                  elem_xml.comment text_content
+                when XML::TextContent::CDATA then
+                  text_content = @db_interface.get_hash_value(@key_builder.content_key, str[1])
+                  elem_xml.cdata text_content
+                end
+              end
             end
           end
-        
-        #This will ad namespace to the current element in xml, raise ArgumentError if given namespace
-        #dos not exist = hasn't been defined in xmlns attribute   
-        xml[namespace] if namespace
-        #Creating element with a given name and attributes, xmlns attributes are automaticaly parsed
-        #as namesace declarations
-        # Note: Underscore is crucial here, it is used to disambiguate ruby internal calls
-        # for detailed information see: http://nokogiri.org/Nokogiri/XML/Builder.html
-        # section special tags
-        xml.send("#{elem_name}_", attrs_hash) do |elem_xml|
-          # This is crucial, Nokogiri uilder automatically adds namespaces into children
-          # if we don't explicily delete them
-          elem_xml.parent.namespace = nil unless namespace
-        
-           if part_keys # if this element is not empty (like <element />
-             part_keys.each do |key_str|
-               type = Transformer::KeyElementBuilder.text_type(key_str)
-               if type
-                 text_content = @db_interface.get_hash_value(@key_builder.content_key, key_str)
-                 case type
-                   when XML::TextContent::PLAIN
-                     elem_xml.text text_content
-                   when XML::TextContent::COMMENT
-                     elem_xml.comment text_content
-                   when XML::TextContent::CDATA
-                     elem_xml.cdata text_content
-                 end
-               else
-                 #Child element
-                 build_node(Transformer::KeyElementBuilder.build_from_s(@key_builder, key_str), elem_xml)
-               end
-             end
-           end
-         end
-    end
+        end
+      end
   end
 end
